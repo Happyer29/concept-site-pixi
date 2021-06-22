7e1489e53051af4598c5aa8ac732011ece487c2e
happyer29 <happyer.present@gmail.com>
2021-06-22 17:59:53 +0300

    first commit

---
+++ scripts/src/event.ts
@@ -0,0 +1,14 @@
+// var loader_num = 0;
+// var loader_inner;
+// var loader_interval;
+//
+// document.addEventListener('DOMContentLoaded', function () {
+//     loader_inner = document.querySelector('.loader');
+//     loader_interval = setInterval(function () {
+//         loader_inner.innerHTML = loader_num;
+//         loader_num++;
+//
+//         if (loader_num >= 101) clearInterval(loader_interval);
+//     }, 20);
+// });
+
